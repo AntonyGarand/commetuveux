4f650f3677e5dd6800a19650105e3281b41928c2
Antony Garand <antony@garand.info>
2016-08-22 13:23:43 -0400

    Ajout de catalogue.php
    Catalogue prend les produits de la table service et les affiche, mais ne
    prends pas en considération les promotions pour le moment.

--- template/config.php
+++ template/config.php
@@ -1,10 +1,13 @@
 <?php
-
 	define('DB_USER','tia16001');
 	define('DB_PASS','bacode');
 	define('DB_NAME','tia16011');
 	define('DB_HOST','127.0.0.1');
 	
+	//Required session variables
 	if(!isset($_SESSION['role'])){
 		$_SESSION['role'] = 'guest';
+	}
+	if(!isset($_SESSION['isLoggedIn'])){
+		$_SESSION['isLoggedIn'] = 0;
 	}
