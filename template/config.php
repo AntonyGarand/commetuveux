1fbb49cde90126a47fe0730c582de2ee50360f0b
Antony Garand <antony@garand.info>
2016-08-22 11:17:15 -0400

    Added meta charset and config

--- template/config.php
+++ template/config.php
@@ -4,3 +4,7 @@
 	define('DB_PASS','bacode');
 	define('DB_NAME','tia16011');
 	define('DB_HOST','127.0.0.1');
+	
+	if(!isset($_SESSION['role'])){
+		$_SESSION['role'] = 'guest';
+	}
--- template/header.inc.php
+++ template/header.inc.php
@@ -6,6 +6,7 @@
 ?>
 <!DOCTYPE HTML>
 <head>
+	<meta charset="UTF-8"/>
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
 <body>
