ebb576292dab3ed1c9413729b2963a9d069bb1c3
Antony Garand <antony@garand.info>
2016-08-25 10:36:39 -0400

    Fixed config

--- template/config.php
+++ template/config.php
@@ -1,15 +1,14 @@
 <?php
-    die('testIndex');
-    /*define('DB_USER','tia16001');
+    define('DB_USER','tia16001');
     define('DB_PASS','bacode');
     define('DB_NAME','tia16011');
-    define('DB_HOST','127.0.0.1');*/
+    define('DB_HOST','127.0.0.1');
 
-    /* TODO: Move to config. Put there for debugging purposes */
-    define('DB_USER', 'root');
+    //Antony's PC debugging values
+    /*define('DB_USER', 'root');
     define('DB_PASS', '');
-    define('DB_NAME', 'infoplusplus');
-    define('DB_HOST', '127.0.0.1');
+    define('DB_NAME', 'projet1');
+    define('DB_HOST', '127.0.0.1');*/
     //Required session variables
     if (!isset($_SESSION['role'])) {
         $_SESSION['role'] = 'guest';
