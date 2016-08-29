4de0a542c1940b988ada4176280cf3cd31ec0f4a
Antony Garand <antony@garand.info>
2016-08-29 11:01:01 -0400

    Update profil.php, added few things
    We can now update our profile in profil.php, admins are also redirected
    to prevent weird bugs
    Added the references images and updated labs in /dev
    Created a basic ajax folder with a .htaccess redirecting to index.php

---
+++ ajax/index.php
@@ -0,0 +1,6 @@
+<?php 
+    if(!isset($_GET['url'])){
+        die('Invalid action');
+    } else {
+        
+    }
--- profil.php
+++ profil.php
@@ -12,132 +12,222 @@ function generateErrorMessage($suffix)
         'email' => "Le courriel $suffix",
         'confirmEmail' => "Le courriel de confirmation $suffix",
         'password' => "Le mot de passe $suffix",
-        'confirmPassword' => "Le mot de passe de confirmation $suffix",
+        'confirmPassword' => "Le mot de passe de confirmation $suffix"
     );
 }
+$cleanNames = array(
+    'firstName' => "Le prénom",
+    'lastName' => "le nom",
+    'civicNo' => "Le numéro civique",
+    'street' => "Le nom de la rue",
+    'city' => 'La ville choisie est manquante ou invalide',
+    'zipCode' => "Le code postal",
+    'phone' => "Le numéro de telephone",
+    'email' => "Le courriel",
+    'confirmEmail' => "Le courriel de confirmation",
+    'password' => "Le mot de passe",
+    'confirmPassword' => "Le mot de passe de confirmation"
+);
+
 //Loading the cities first as we need them in the profile validation (Check if id exists)
 $citiesQuery = 'SELECT * FROM ville ORDER BY pk_ville';
 $cities = $db->query($citiesQuery)->fetchAll();
 
+//Setting the user variable if we're logged in
 $user = false;
-if (isset($_POST['userId']) && is_numeric($_POST['userId'])) {
-    if (intval($_POST['userId']) !== intval($_SESSION['userId'])) {
-        require_once 'template/navbar.inc.php';
-        die('<h1>Erreurs lors de la mise à jour!</h1><h3>Veuillez vous reconnecter.</h3>');
-    } else {
-        die('WIP');
-    }
-}
 if ($_SESSION['userId'] !== 0) {
     $userSelectQuery = "SELECT * FROM utilisateur JOIN client ON client.fk_utilisateur = pk_utilisateur JOIN adresse ON adresse.pk_adresse = client.fk_adresse WHERE pk_utilisateur = {$_SESSION['userId']}";
     $userSelectStmt = $db->query($userSelectQuery);
     if ($userSelectStmt->rowCount() === 1) {
         $user = $userSelectStmt->fetch();
+        //Admins don't have a profile! redirect them to prevent bugs and errors
+        if($user['administrateur'] == 1){
+            die(header("Location: index.php"));
+        }
     }
 }
 
 if (isset($_POST['profil'])) {
-    //Form is being sent, validate the content and try to create a user
-    $required = array('firstName', 'lastName', 'civicNo', 'street', 'city', 'zipCode', 'phone', 'email', 'confirmEmail', 'password', 'confirmPassword');
-    $errors = validatePost($required);
-    if (empty($errors)) {
+    function validateInformation(){
+        global $db, $cities, $cleanNames;
+        //Form is being sent, validate the content and try to create a user
+        $required = array('firstName', 'lastName', 'civicNo', 'street', 'city', 'zipCode', 'phone', 'email', 'confirmEmail', 'password', 'confirmPassword');
+        $errors = validatePost($required);
+        if(!empty($errors)){
+            $suffix = 'est manquant ou invalide';
+            $errorMessage = generateErrorMessage($suffix);
+            $errorsClean = array();
+            foreach ($errors as $error) {
+                $errorsClean[] = $errorMessage[$error];
+            }
+            return $errorsClean;
+        }
         //All values are present and strings, let's keep checking them
-        $emailCheckQuery = "SELECT * FROM utilisateur WHERE courriel = :courriel AND pk_utilisateur is not {$_SESSION['userId']}";
+        $emailCheckQuery = "SELECT * FROM utilisateur WHERE courriel = :courriel AND pk_utilisateur != {$_SESSION['userId']}";
         $emailCheckStmt = $db->prepare($emailCheckQuery);
         //Server error!
         if (!$emailCheckStmt->execute(array(':courriel' => $_POST['email']))) {
             $errorsClean = array('Erreur du serveur! Veuillez réessayer.');
-        } else {
-            //The email already exists in the database
-            if ($emailCheckStmt->rowCount() > 0) {
-                $errorsClean = array('Le courriel est déjà pris! Veuillez réessayer.');
-            } else {
-                //Validating the info sent by the client
-                $cityExists = false;
-                $_POST['city'] = intval($_POST['city']);
-                //Looping through the cities. If one has pk_ville = city sent, it's valid!
-                foreach ($cities as$city) {
-                    if ($city['pk_ville'] == $_POST['city']) {
-                        $cityExists = true;
-                        break;
-                    }
-                }
-                if (!$cityExists) {
-                    $errorsClean = array("La cité n'existe pas! Veuillez réessayer");
-                } else {
-                    //City exists, now check the lengths and values sent
-                    $lengthAndTypeValidator = array(
-                        'firstName' => array('minLength' => 2, 'maxLength' => 75, 'type' => 'string'),
-                        'lastName' => array('minLength' => 2, 'maxLength' => 75, 'type' => 'string'),
-                        'civicNo' => array('minLength' => 1, 'maxLength' => 10, 'type' => 'string'),
-                        'zipCode' => array('minLength' => 6, 'maxLength' => 6, 'type' => 'string'),
-                        'street' => array('minLength' => 2, 'maxLength' => 75, 'type' => 'string'),
-                        'phone' => array('minLength' => 10, 'maxLength' => 20, 'type' => 'string'),
-                        'email' => array('minLength' => 5, 'maxLength' => 100, 'type' => 'string'),
-                        'password' => array('minLength' => 8, 'maxLength' => 100, 'type' => 'string'),
-                    );
-                    $errorsClean = array();
-                    $errorMessage = generateErrorMessage("n'a pas une longueur valide!");
-                    if ($_POST['password'] !== $_POST['confirmPassword']) {
-                        $errorsClean[] = 'Les deux mots de passe ne correspondent pas!';
-                    }
-                    if ($_POST['email'] !== $_POST['confirmEmail']) {
-                        $errorsClean[] = 'Les deux courriels ne correspondent pas!';
-                    }
-                    foreach ($lengthAndTypeValidator as $key => $validation) {
-                        $length = strlen($_POST[$key]);
-                        if ($length < $validation['minLength'] || $length > $validation['maxLength']) {
-                            $errorsClean[] = $errorMessage[$key];
-                            echo $key.'<br/>';
-                        }
-                    } //End of foreach key validation
-                    //All values are good, create the database entries
-                    if (empty($errorsClean)) {
-                        //TODO: Salt+Hash the password! Plaintext is bad
-                        $userCreateQuery = "INSERT INTO `utilisateur` (`pk_utilisateur`, `courriel`, `mot_de_passe`, `administrateur`) VALUES (NULL, :email, :password, '0')";
-                        $adressCreateQuery = 'INSERT INTO `adresse` (`pk_adresse`, `no_civique`, `rue`, `fk_ville`, `code_postal`) VALUES (NULL, :civicNo, :street, :city, :zipCode)';
-                        $clientCreateQuery = 'INSERT INTO `client` (`pk_client`, `fk_utilisateur`, `prenom`, `nom`, `fk_adresse`, `telephone`, `infolettre`) VALUES (NULL, :userId, :prenom, :nom, :adressId, :phone, :wantSpam)';
-                        $adressCreateStmt = $db->prepare($adressCreateQuery);
-                        $adressCreateStmt->execute(array(
-                            ':civicNo' => $_POST['civicNo'],
-                            ':street' => $_POST['street'],
-                            ':city' => $_POST['city'],
-                            ':zipCode' => $_POST['zipCode'],
-                        ));
-                        $adressId = $db->lastInsertId();
-                        $userCreateStmt = $db->prepare($userCreateQuery);
-                        $userCreateStmt->execute(array(
-                            ':email' => $_POST['email'],
-                            ':password' => $_POST['password'],
-                        ));
-                        $userId = $db->lastInsertId();
-
-                        $clientCreateStmt = $db->prepare($clientCreateQuery);
-                        $clientCreateStmt->execute(array(
-                            ':userId' => $userId,
-                            ':prenom' => $_POST['firstName'],
-                            ':nom' => $_POST['lastName'],
-                            ':adressId' => $adressId,
-                            ':phone' => $_POST['phone'],
-                            ':wantSpam' => empty($_POST['sendPromo']) ? 0 : 1,
-                        ));
-                        die('Le compte a été créer!');
-                        //TODO: Header index.php?
-                    }
-                } //End of city does exist condition
+            die(var_dump($emailCheckStmt));
+            return $errorsClean;
+        }
+        //The email already exists in the database
+        if ($emailCheckStmt->rowCount() > 0) {
+            $errorsClean = array('Le courriel est déjà pris! Veuillez réessayer.');
+            return $errorsClean;
+        }
+        //Validating the info sent by the client
+        $cityExists = false;
+        $_POST['city'] = intval($_POST['city']);
+        //Looping through the cities. If one has pk_ville = city sent, it's valid!
+        foreach ($cities as$city) {
+            if ($city['pk_ville'] == $_POST['city']) {
+                $cityExists = true;
+                break;
             }
         }
-    } else {
-        $suffix = 'est manquant ou invalide';
-        $errorMessage = generateErrorMessage($suffix);
-        $errorsClean = array();
-        foreach ($errors as $error) {
-            $errorsClean[] = $errorMessage[$error];
+        if (!$cityExists) {
+            $errorsClean = array("La cité n'existe pas! Veuillez réessayer");
+            return $errorsClean;
         }
-    } //End of profile validation
+        //City exists, now check the lengths and values sent
+        $lengthAndTypeValidator = array(
+            'firstName' => array('minLength' => 2, 'maxLength' => 75, 'type' => 'string'),
+            'lastName' => array('minLength' => 2, 'maxLength' => 75, 'type' => 'string'),
+            'civicNo' => array('minLength' => 1, 'maxLength' => 10, 'type' => 'string'),
+            //Regex: As postal code can't contain  D, F, I, O, Q, or U, and cannot start with W or Z, make a manual regex with the charcters
+            'zipCode' => array('minLength' => 6, 'maxLength' => 6, 'type' => 'string', 'regex'=>'/^[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/'),
+            'street' => array('minLength' => 2, 'maxLength' => 75, 'type' => 'string'),
+            'phone' => array('minLength' => 10, 'maxLength' => 20, 'type' => 'string', 'regex'=>'/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/'),
+            'email' => array('minLength' => 5, 'maxLength' => 100, 'type' => 'string'),
+            'password' => array('minLength' => 8, 'maxLength' => 100, 'type' => 'string'),
+        );
+        $errorsClean = array();
+        $errorMessage = generateErrorMessage("n'a pas une longueur valide!");
+        if ($_POST['password'] !== $_POST['confirmPassword']) {
+            $errorsClean[] = 'Les deux mots de passe ne correspondent pas!';
+        }
+        if ($_POST['email'] !== $_POST['confirmEmail']) {
+            $errorsClean[] = 'Les deux courriels ne correspondent pas!';
+        }
+        //Email format validation
+        if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
+            $errorsClean[] = 'Le courriel ne respecte pas le format demandé!';
+        }
+        foreach ($lengthAndTypeValidator as $key => $validation) {
+            $length = strlen($_POST[$key]);
+            if ($length < $validation['minLength'] || $length > $validation['maxLength']) {
+                $errorsClean[] = $errorMessage[$key];
+            }
+            if(isset($validation['regex'])){
+                if(preg_match($validation['regex'],$_POST[$key]) !== 1){
+                    $errorsClean[] = $cleanNames[$key] . ' ne respecte pas le format demandé!';
+                };
+            }
+        } //End of foreach key validation
+        if(!empty($errorsClean)){
+            return $errorsClean;
+        }
+        return array();
+    }
+    function createNewUser(){
+        global $db;
+        //User creation!
+        //TODO: Salt+Hash the password! Plaintext is bad
+        $userCreateQuery = "INSERT INTO `utilisateur` (`pk_utilisateur`, `courriel`, `mot_de_passe`, `administrateur`) VALUES (NULL, :email, :password, '0')";
+        $adressCreateQuery = 'INSERT INTO `adresse` (`pk_adresse`, `no_civique`, `rue`, `fk_ville`, `code_postal`) VALUES (NULL, :civicNo, :street, :city, :zipCode)';
+        $clientCreateQuery = 'INSERT INTO `client` (`pk_client`, `fk_utilisateur`, `prenom`, `nom`, `fk_adresse`, `telephone`, `infolettre`) VALUES (NULL, :userId, :prenom, :nom, :adressId, :phone, :wantSpam)';
+        $adressCreateStmt = $db->prepare($adressCreateQuery);
+        $adressCreateStmt->execute(array(
+            ':civicNo' => $_POST['civicNo'],
+            ':street' => $_POST['street'],
+            ':city' => $_POST['city'],
+            ':zipCode' => $_POST['zipCode'],
+        ));
+        $adressId = $db->lastInsertId();
+        $userCreateStmt = $db->prepare($userCreateQuery);
+        //TODO: Sanitize email when echoing it out, as "><script>alerT(1);</script>"@test.ca is a valid email!
+        //Anything in the form "(.*)"@domain.com is valid, which could lead to XSS if not sanitized properly.
+        $userCreateStmt->execute(array(
+            ':email' => $_POST['email'],
+            ':password' => $_POST['password'],
+        ));
+        $userId = $db->lastInsertId();
+
+        $clientCreateStmt = $db->prepare($clientCreateQuery);
+        $clientCreateStmt->execute(array(
+            ':userId' => $userId,
+            ':prenom' => $_POST['firstName'],
+            ':nom' => $_POST['lastName'],
+            ':adressId' => $adressId,
+            ':phone' => $_POST['phone'],
+            ':wantSpam' => empty($_POST['sendPromo']) ? 0 : 1,
+        ));
+        //New account created
+        header("Location: index.php");
+    }
+
+    function updateUser(){
+        global $db;
+        $selectCompleteUserStmt = "SELECT * from client JOIN utilisateur on utilisateur.pk_utilisateur = client.fk_utilisateur JOIN adresse ON adresse.pk_adresse = client.fk_adresse WHERE utilisateur.pk_utilisateur = {$_SESSION['userId']}";
+        $user = $db->query($selectCompleteUserStmt)->fetch();
+
+        $fields = array(
+            'firstName'=>'prenom',
+            'lastName'=>'nom',
+            'phone'=>'telephone',
+            'email'=>'courriel',
+            'sendPromo'=>'infolettre',
+            'password'=>'mot_de_passe',
+            'civicNo'=>'no_civique',
+            'street'=>'rue',
+            'zipCode'=>'code_postal'
+        );
+
+        $clientUpdateQuery = "UPDATE `client` SET `prenom` = :firstName, `nom` = :lastName, `telephone` = :phone WHERE `client`.`pk_client` = {$user['pk_client']}";
+        $adressUpdateQuery = "UPDATE `adresse` SET `no_civique` = :civicNo, `rue` = :street, `code_postal` = :zipCode, `fk_ville` = :ville WHERE `adresse`.`pk_adresse` = {$user['pk_adresse']}";
+        $userUpdateQuery = "UPDATE `utilisateur` SET `courriel` = :email, `mot_de_passe` = :password WHERE `utilisateur`.`pk_utilisateur` = {$user['pk_utilisateur']}";
+        $clientUpdateStmt=$db->prepare($clientUpdateQuery);
+        $clientUpdateStmt->execute(array(
+            ':firstName'=>$_POST['firstName'],
+            ':lastName'=>$_POST['lastName'],
+            ':phone'=>$_POST['phone']
+        ));
+        $adressUpdateStmt=$db->prepare($adressUpdateQuery);
+        $adressUpdateStmt->execute(array(
+            ':civicNo'=>$_POST['civicNo'],
+            ':street'=>$_POST['street'],
+            ':ville'=>$_POST['ville'],
+            ':zipCode'=>$_POST['zipCode']
+        ));
+        $userUpdateStmt=$db->prepare($userUpdateQuery);
+        $userUpdateStmt->execute(array(
+            ':email'=>$_POST['email'],
+            ':password'=>$_POST['password']
+        ));
+        header("Location: index.php");
+        die();
+    }
+    $errorsClean = validateInformation();
+
+    if(empty($errorsClean)){
+        if (isset($_POST['userId']) && is_numeric($_POST['userId'])) {
+            if (intval($_POST['userId']) !== intval($_SESSION['userId'])) {
+                require_once 'template/navbar.inc.php';
+                echo'<h1>Erreurs lors de la mise à jour!</h1><h3>Veuillez vous reconnecter.</h3>';
+                require_once'template/footer.inc.php';
+                die();
+            } else {
+                updateUser();
+            }
+        } else {
+            createNewUser();
+        }
+    }
+
 } //End of profile confirmation
 
-if (empty($_POST)) {
+if (empty($_POST) && $user !== false) {
     $firstName = htmlspecialchars(htmlspecialchars_decode($user['prenom']));
     $lastName = htmlspecialchars(htmlspecialchars_decode($user['nom']));
     $civicNo = htmlspecialchars(htmlspecialchars_decode($user['no_civique']));
@@ -169,8 +259,8 @@ if (empty($_POST)) {
     } else {
         $street = '';
     }
-    if (isset($_POST['cityId']) && is_string($_POST['cityId'])) {
-        $cityId = htmlspecialchars($_POST['cityId']);
+    if (isset($_POST['city']) && is_numeric($_POST['city'])) {
+        $cityId = intval($_POST['city']);
     } else {
         $cityId = '';
     }
@@ -195,6 +285,7 @@ if (empty($_POST)) {
         $emailConfirm = '';
     }
 }
+
 require_once 'template/navbar.inc.php';
 
 /*
