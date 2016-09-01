6d07377f9f4064fab7583c25af1cbbbee5d02d5b
Antony Garand <antony@garand.info>
2016-09-01 10:19:10 -0400

    Fixed indent, added menu links based on role
    In navbar.inc.php

--- template/navbar.inc.php
+++ template/navbar.inc.php
@@ -1,50 +1,39 @@
 <!DOCTYPE HTML>
 <head>
-	<meta charset="UTF-8"/>
-	<link rel="stylesheet" type="text/css" href="css/reset.css"/>
-	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
 <body>
-
-	<nav>
-		<!-- Puts the header BG at 100% -->
-	</nav>
-	
-	<div class="container">
-	
-	<div id="mainMenu">
-		<div id="logo">
+    <nav>
+        <!-- Puts the header BG at 100% -->
+    </nav>
+    <div class="container">
+    <div id="mainMenu">
+        <div id="logo">
             <a href="index.php" class="logo"><img src="img/graphiques/logo.png" alt="Info++" /></a>
-		</div> <!-- end #logo -->
-		<div id="menuNav">
-			<div class="menuOption">
-			<?php 
-                if ($_SESSION['role'] === 'admin') {
-                    //Admin
-                    ?>
-						<a href="logout.php">se d&eacute;connecter</a>
-					<?php
-
-                } elseif ($_SESSION['role'] === 'user') {
-                    //Usager
-                    ?>
-						<a href="panier.php">Mon Panier(<?=$_SESSION['pannierQte']?>)</a>
-						<a href="logout.php">se d&eacute;connecter</a>
-					<?php
-
-                } else {
-                    //Visiteur non authentifié
-                    ?>
-						<a href="login.php">s'identifier</a>
-					<?php
-
-                }
-            ?>
-			</div>
-			<div class="navOption">
-				<a class="nav red" href="catalogue.php">Catalogue</a>
-				<a class="nav yellow" href="profil.php">Profil</a>
-				<input type="text" name="search" value="Recherche"/>
-			</div>
-		</div> <!-- end #menuNav -->
-		</div> <!-- end #mainMenu -->
+        </div> <!-- end #logo -->
+        <div id="menuNav">
+            <div class="menuOption">
+                <?php if ($_SESSION['role'] === 'admin') { //Admin ?>
+                    <a href="logout.php">se d&eacute;connecter</a>
+                <?php } elseif ($_SESSION['role'] === 'user') { //Usager ?>
+                    <a href="panier.php">Mon Panier(<?=$_SESSION['pannierQte']?>)</a>
+                    <a href="logout.php">se d&eacute;connecter</a>
+                <?php } else { //Visiteur non authentifié ?>
+                    <a href="login.php">s'identifier</a>
+                <?php } ?>
+            </div>
+            <div class="navOption">
+                <?php if ($_SESSION['role'] === 'admin') { //Admin ?>
+                    <a class="nav red" href="service.php">Service</a>
+                    <a class="nav yellow" href="facture.php">Facture</a>
+                    <input type="text" name="search" value="Recherche"/>
+                <?php } elseif ($_SESSION['role'] === 'user') { //Usager ?>
+                    <a class="nav red" href="catalogue.php">Catalogue</a>
+                    <a class="nav yellow" href="profil.php">Profil</a>
+                    <input type="text" name="search" value="Recherche"/>
+                <?php } ?>
+            </div>
+        </div> <!-- end #menuNav -->
+    </div> <!-- end #mainMenu -->
