937a892f8d213c267ad2dcf126012b76c9662e01
amy-f <frappier.amelie@gmail.com>
2016-10-30 10:20:15 -0400

    changements mineurs panier

--- panier.php
+++ panier.php
@@ -3,19 +3,23 @@ require_once 'template/header.inc.php';
 require_once "librairies/DPayPal.php";
 $paypal = new DPayPal();
 
+//save token in session variable
+if(isset($_GET['token'])) {
+	$token=$_GET["token"];//Returned by paypal, you can save this in SESSION too
+	$_SESSION['paypaltoken'] = $token;
+}
+
 //Fait le paiement si l'adresse de retour à été renvoyée au bon endroit
 if (isset($_GET['confirmpayment']) && $_GET['confirmpayment']) {
 	
-	$token=$_GET["token"];//Returned by paypal, you can save this in SESSION too
-	
-	$requestParams = array('TOKEN' => $token);
+	$requestParams = array('TOKEN' => $_SESSION['paypaltoken']);
 	
 	$response = $paypal->GetExpressCheckoutDetails($requestParams);
-	$payerId=$response["PAYERID"];//Payer id returned by paypal
+	$payerId=$_GET["PAYERID"];//Payer id returned by paypal
 	
 	//Create request for DoExpressCheckoutPayment
 	$requestParams=array(
-		"TOKEN"=>$token,
+		"TOKEN"=>$_SESSION['paypaltoken'],
 		"PAYERID"=>$payerId,
 		"PAYMENTREQUEST_0_AMT"=>"20",//Payment amount. This value should be sum of of item values, if there are more items in order
 		"PAYMENTREQUEST_0_CURRENCYCODE"=>"EUR",//Payment currency
@@ -26,7 +30,7 @@ if (isset($_GET['confirmpayment']) && $_GET['confirmpayment']) {
 	
 	if(is_array($transactionResponse) && $transactionResponse["ACK"]=="Success"){//Payment was successfull
 		//Successful Payment
-		//empty cart and show succesful message
+		//empty cart and show successful message
 	}
 	else{
 		//Failure
@@ -52,9 +56,8 @@ require_once 'template/navbar.inc.php';
 <body>
     <div class="contenu">
 
-        <br><br>	
-    	<hr>
         
+		
 	    <h2>Paiement</h2>
 		
 		<form id="checkout" name="checkout" method="post" action="payment.php">
